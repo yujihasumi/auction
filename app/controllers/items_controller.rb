2e518ef38f51eb9c831c897116dc9c23063dddd3
Yuji <mtcook3754@gmail.com>
2019-05-02 15:32:35 +0900

    登録完成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,33 @@ class ItemsController < ApplicationController
     def index
       @items = Item.all
     end
+
+    def new
+      @item=Item.new
+    end
+
+    def create
+
+      @item =Item.new(item_params)
+      @item.save
+      redirect_to @item
+
+    end
+    def edit
+      @item=Item.find(params[:id])
+    end
+
+    def update
+      @item=Item.find(params[:id])
+      @item.update(item_params)
+
+      redirect_to @item
+
+
+    end
+
+    private
+    def item_params
+  params.require(:item).permit(:name,:price,:seller,:description,:email,:image_url)
+end
 end
